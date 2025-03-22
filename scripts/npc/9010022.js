ec4147f5aee5c91814ef07edb65c2c75405d6131
anon <anon@example.com>
2025-03-22 10:58:48 -0700

    Ongoing changes...

--- scripts/npc/9010022.js
+++ scripts/npc/9010022.js
@@ -1,4 +1,4 @@
-// AdventureMS Dimensional Mirror
+// AdventureMS Gatekeeper Omnar
 
 var status;
 
@@ -8,73 +8,60 @@ function action(mode, type, selection) { if (mode == 1) {status++;} else {status
 
         else if (status == 0)
         {
-            if (cm.getLevel() < 20)
+            // Create empty string to store locations available
+            var selStr = "";
+
+            // Check that they've at least cleared zone 1
+            if (cm.getZoneProgress() > 0)
             {
-                cm.sendOk("You haven't unlocked any warps!");
-                // cm.sendDimensionalMirror("#-1# There is no place for you to transport to from here.");
-                cm.dispose();
+                // Get ZoneProgress and create the available warps
+                switch (cm.getZoneProgress())
+                {
+                    case 3:
+                        selStr += "#0# Kora Garden";
+                        selStr += "#1# Kerning City";
+                        selStr += "#2# Stoneweaver Village";
+                        break;
+
+                    case 2:
+                        selStr += "#0# Kora Garden";
+                        selStr += "#1# Kerning City";;
+                        break;
+
+                    case 1:
+                        selStr += "#0# Kora Garden";;
+                        break;
+                }
+
+                // Send the completed string for the dimensional mirror to handle
+                cm.sendDimensionalMirror(selStr);
             }
 
+            // They haven't cleared zone 1
             else
             {
-                var selStr = "";
-                if (cm.getLevel() >= 20 && cm.getLevel() <= 30)
-                {
-                    selStr += "#0# Ariant Coliseum";
-                }
-
-                if (cm.getLevel() >= 25) {
-                    selStr += "#1# Mu Lung Dojo";
-                }
-
-                if (cm.getLevel() >= 30 && cm.getLevel() <= 50) {   // MC 1 & 2 recalled thanks to ---
-                    selStr += "#2# Monster Carnival 1";
-                }
-
-                if (cm.getLevel() >= 51 && cm.getLevel() <= 70) {
-                    selStr += "#3# Monster Carnival 2";
-                }
-
-                /*
-                if (cm.getLevel() >= 40) { NOT IMPLEMENTED
-                    selStr += "#5# Nett's Pyramid"; 
-                } 
-
-                if (cm.getLevel() >= 25 && cm.getLevel() <= 30) { NOT IMPLEMENTED
-                    selStr += "#6# Construction Site"; 
-                } 
-                */
-
-                cm.sendDimensionalMirror(selStr);
+                cm.sendOk("You've not unlocked any warps! Get after it!");
+                cm.dispose();
             }
         }
 
+        // They've chosen a map to warp to
         else if (status == 1)
         {
-            cm.getPlayer().saveLocation("MIRROR");
             switch (selection)
             {
-                case 0:
-                    cm.warp(980010000, 3);
+                case 2:
+                    cm.warp(101040002, 0);
                     break;
                 case 1:
-                    cm.warp(925020000, 0);
+                    cm.warp(103000000, 0);
                     break;
-                case 2:
-                    cm.getPlayer().saveLocation("MONSTER_CARNIVAL");
-                    cm.warp(980000000, 3);
-                    break;
-                case 3:
-                    cm.getPlayer().saveLocation("MONSTER_CARNIVAL");
-                    cm.warp(980030000, 3);
-                    break;
-                case 5:
-                    cm.warp(926010000, 4);
-                    break;
-                case 6:
-                    cm.warp(910320000, 2);
+                case 0:
+                    cm.warp(104040000, 0);
                     break;
             }
+
+            // Kill the convo
             cm.dispose();
         }
     }
