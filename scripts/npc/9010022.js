dfce94a48a92b265f0dfeb6dd768a7258ab00a6f
anon <anon@example.com>
2025-03-23 18:56:15 -0700

    Ongoing changes...

--- scripts/npc/9010022.js
+++ scripts/npc/9010022.js
@@ -1,6 +1,8 @@
 // AdventureMS Omnar
 
 var status;
+var tokens[3997006, 3997007, 3997008];
+var quests[1007, 1008, 1009];
 
 // Start the conversation
 function start() {status = -1; action(1,0,0);}
@@ -8,56 +10,90 @@ function action(mode, type, selection) { if (mode == 1) {status++;} else {status
 
         else if (status == 0)
         {
-            // Check if they have a token
-            if (cm.haveItem(3997006) || cm.haveItem(3997007) || cm.haveItem(3997008))
+            // Loop through tokens array
+            for (var i = 0; i < tokens.length; i++)
             {
-                // Check if they've already turned in at least one token
-                if (cm.getQuestStatus(1007) == 2 || cm.getQuestStatus(1008) == 2 || cm.getQuestStatus(1009) == 2)
+                // If we have a token in our possession, stop
+                if (cm.haveItem(tokens[i]))
                 {
-                    // They've turned in at least one token
-                    cm.sendOk("Ah! Another one! Yes, it looks delicious. Quick throw it in!\r\n\r\n(Om nom nom 'crunch', 'munch'...)\r\n\r\nThat WAS delicious! I suppose I could open up another passage for you...");
-                    cm.dispose();
-                }
+                    // Loop through the quest array
+                    for (var j = 0; j < quests.length; j++)
+                    {
+                        // If we have completed any quest, it's our 2nd time back
+                        if (cm.getQuestStatus(quests[j]) == 2)
+                        {
+                            // Remove the item
+                            cm.gainItem(tokens[i], -1);
 
-                // This is their first token to turn in
-                else
-                {
-                    cm.sendOk("Ooooooo, a #rWarp Token#k!\r\n\r\nWell, that's what your lot call 'em anyway. Us wardens call 'em super-tasty, incredibly-delicious, interdimensional biscuits! #rS.T.I.D.I.B#k\r\n\r\nAnyway, here's how it works. You toss it in my mouth, yes, the portal is my mouth. I munch and crunch it up. Voila, you get a new fast warp! Throw it in there!\r\n\r\n(Om nom nom 'crunch', 'munch'...)\r\n\r\nDelicious! Just like that, you get a new warp! Thanks!");
+                            // Complete the quest
+                            cm.completeQuest(quests[i]);
+
+                            // They've turned in at least one token
+                            cm.sendOk("Ah! Another one! Yes, it looks delicious. Quick throw it in!\r\n\r\n(Om nom nom 'crunch', 'munch'...)\r\n\r\nThat WAS delicious! I suppose I could open up another passage for you...");
+                            cm.dispose();
+                            break;
+                        }
+                    }
+
+                    // Check if it's our first time here
+                    if (j >= quests.length)
+                    {
+                        // Remove the item
+                        cm.gainItem(tokens[i], -1);
+
+                        // Complete the quest
+                        cm.completeQuest(quests[i]);
+
+                        // It's our first time here
+                        cm.sendOk("Ooooooo, a #rWarp Token#k!\r\n\r\nWell, that's what your lot call 'em anyway. Us wardens call 'em super-tasty, incredibly-delicious, interdimensional biscuits! #r#eS.T.I.D.I.B#n#k's\r\n\r\nAnyway, here's how it works. You toss it in my mouth, yes, the portal is my mouth. I munch and crunch it up. Voila, you get a new fast warp! Throw it in there!\r\n\r\n(Om nom nom 'crunch', 'munch'...)\r\n\r\nDelicious! Just like that, you get a new warp! Thanks!");
+                        cm.dispose();
+                        break;
+                    }
                 }
             }
 
-            // Check that they've at least cleared zone 1
-            else if (cm.getQuestStatus(1007) == 2 || cm.getQuestStatus(1008) == 2 || cm.getQuestStatus(1009) == 2)
+            // We did not have a token
+            if (i >= tokens.length)
             {
-                // Create empty string to store locations available
-                var selStr = "";
-
-                // Kora Check
-                if (cm.getQuestStatus(1007) == 2)
+                // If we have completed any quest, open the warper
+                for (var j = 0; j < quests.length; j++)
                 {
-                    selStr += "#0# Kora Garden";
-                }
+                    // If we have completed any quest, open the warper
+                    if (cm.getQuestStatus(quests[j]) == 2)
+                    {
+                        // Create empty string to store locations available
+                        var selStr = "";
 
-                // Kerning Check
-                if (cm.getQuestStatus(1008) == 2)
-                {
-                    selStr += "#1# Kerning City";
-                }
+                        // Kora Check
+                        if (cm.getQuestStatus(1007) == 2)
+                        {
+                            selStr += "#0# Kora Garden";
+                        }
 
-                // Check Quests, one by one
-                if (cm.getQuestStatus(1009) == 2)
-                {
-                    selStr += "#2# Stoneweaver Village";
-                }
+                        // Kerning Check
+                        if (cm.getQuestStatus(1008) == 2)
+                        {
+                            selStr += "#1# Kerning City";
+                        }
 
-                // Send the completed string for the dimensional mirror to handle
-                cm.sendDimensionalMirror(selStr);
+                        // Check Quests, one by one
+                        if (cm.getQuestStatus(1009) == 2)
+                        {
+                            selStr += "#2# Stoneweaver Village";
+                        }
+
+                        // Send the completed string for the dimensional mirror to handle
+                        cm.sendDimensionalMirror(selStr);
+                        cm.dispose();
+                        break;
+                    }
+                }
             }
 
-            // They haven't cleared zone 1
+            // They have no tokens, and they haven't completed any quests
             else
             {
-                cm.sendOk("You've not unlocked any warps! Get after it!");
+                cm.sendOk("Go find me some of those tasty #r#eS.T.I.D.I.B#n#k's!");
                 cm.dispose();
             }
         }
