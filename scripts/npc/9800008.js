a2ba838c68f60da030096aea32066da29c418fe2
anon <anon@example.com>
2025-03-26 11:56:19 -0700

    Ongoing changes...

--- scripts/npc/9800008.js
+++ scripts/npc/9800008.js
@@ -1,13 +1,7 @@
-// Author: Pepa
+// AdventureMS White Lady
 
-// AdventureMS - White Lady
-
-function start()
-{
-    status = -1;
-    action(1,0,0);
-}
-
+// Standard Status Code
+function start() {status = -1; action(1,0,0);}
 function action(mode, type, selection) { if (mode == 1) {status++;} else {status--;} if (status == -1) {cm.dispose();}
 
     // Intro and Quest Status Check
@@ -19,39 +13,51 @@ function action(mode, type, selection) { if (mode == 1) {status++;} else {status
             // Already earned the medal
             cm.sendOk("Would you really have saved me? You would have been risking your own life...\r\n\r\nKeep that #rmedal#k, it's one of five very rare medals to be traded in...");
             cm.dispose();
-        } else if (cm.getQuestStatus(1000) == 0)
+        }
+
+        else if (cm.getQuestStatus(1000) == 0)
         {
             // Start Quest to avoid starting prompt everytime
             cm.startQuest(1000);
+
             // Intro
-            cm.sendNext("(at first she barely notices you, she slowly turns around and stares intently at you)\r\n\r\nI've not seen you before...are you the one that killed me?...");
+            cm.sendNext("(at first she barely notices you, she slowly turns around and stares intently at you)\r\n\r\nI've not seen you before...are you the one that killed me?...\r\n\r\n#rNOTE:#k This is NOT related to the Blood Statue...");
         }
+
         else
         {
             // Reintro w/ gear check
             cm.sendNext("You've come back...let's see if you've improved your gear...");
         }
     }
+
     // Equipment HP Check and response
     else if (status == 1)
     {
         // Store vars for use
         var equipStats = cm.getPlayer().getEquipStats(); // Returns all stats for equipment
-        var goalHP = 2000; // This is the goal HP to have on gear
+        var goalHP = 6250; // This is the goal HP to have on gear
         var hpdif = goalHP - equipStats[0]; // Calculate the difference
 
         // Checks if we have passed expected max/max (based on lvl 120) 60% for DEF scrolls on all slots
         if (equipStats[0] >= goalHP)
         {
             cm.sendAcceptDecline("O.o? ...You've done it... You would have survived the attack.\r\n\r\n...but...would you have protected me?");
-        } else
+        }
+
+        // They've not got enough HP on their gear yet
+        else
         {
-            if (hpdif > 6000) {cm.sendOk("...no...you don't look like the one that killed me, they were much stronger...\r\n\r\nYou would not have survived either. If your #e#requipment had more HP#n#k you may have been able to.\r\n\r\n#rYou are not close at all...");}
-            else if (hpdif > 4500) {cm.sendOk("...no...you don't look like the one that killed me, they were much stronger...\r\n\r\nYou would not have survived either. If your #e#requipment had more HP#n#k you may have been able to.\r\n\r\n#rYou have a long way to go still...");}
-            else if (hpdif > 3000) {cm.sendOk("...no...you don't look like the one that killed me, they were much stronger...\r\n\r\nYou would not have survived either. If your #e#requipment had more HP#n#k you may have been able to.\r\n\r\n#rKeep going, your gear is getting better...");}
-            else if (hpdif > 1500) {cm.sendOk("...no...you don't look like the one that killed me, they were much stronger...\r\n\r\nYou would not have survived either. If your #e#requipment had more HP#n#k you may have been able to.\r\n\r\n#rYou are getting much closer, but still have a ways to go...");}
-            else if (hpdif > 750) {cm.sendOk("...no...you don't look like the one that killed me, they were much stronger...\r\n\r\nYou would not have survived either. If your #e#requipment had more HP#n#k you may have been able to.\r\n\r\n#rYou are pretty close now, just not quite there...");}
-            else if (hpdif > 0) {cm.sendOk("...no...you don't look like the one that killed me, they were much stronger...\r\n\r\nYou would not have survived either. If your #e#requipment had more HP#n#k you may have been able to.\r\n\r\n#rYou are very close... #r" + hpdif + " HP#k left to go...");}
+            // Store the default text
+            var defaultString = "...no...you don't look like the one that killed me, they were much stronger...\r\n\r\nYou would not have survived either. If your #e#requipment had more HP#n#k you may have been able to.\r\n\r\n";
+
+            if (hpdif <= 750) {cm.sendOk(defaultString + "#rYou are very close... " + hpdif + " HP left to go...#k");}
+            else if (hpdif <= 1500) {cm.sendOk(defaultString + "#rMore than three quarters of the way there...#k");}
+            else if (hpdif <= 3000) {cm.sendOk(defaultString + "#rKeep going, you are past halfway...#k");}
+            else if (hpdif <= 4500) {cm.sendOk(defaultString + "#rYou have a long way to go still...just past a quarter of the way...#k");}
+            else {cm.sendOk(defaultString + "#rYou are not close at all...#k");}
+
+            // Kill the convo
             cm.dispose();
         }
 
