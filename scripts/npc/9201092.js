387c3e73e09cf7385be04f1b35ed8b5ba5077555
anon <anon@example.com>
2025-03-25 22:51:38 -0700

    Ongoing changes...

--- scripts/npc/9201092.js
+++ scripts/npc/9201092.js
@@ -309,7 +309,7 @@ function action(mode, type, selection) { if (mode == 1) {status++;} else {status
                 var Item = Java.type('client.inventory.Item');
 
                 // Remove the item from the player’s inventory
-                var position = cm.getPlayer().getInventory().getPosition(selectedItemId);
+                var position = cm.getPlayer().getInventory(USE).getPosition(selectedItemId);
              }
 
              else
