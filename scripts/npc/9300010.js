e1eb7b6566a287ba68fcb031915ef6282e7d9228
anon <anon@example.com>
2025-03-26 20:30:51 -0700

    Ongoing changes...

--- scripts/npc/9300010.js
+++ scripts/npc/9300010.js
@@ -2,6 +2,9 @@
 
 // AdventureMS Mr Moneybags
 
+// Declare Global Variables
+selectionSlot = 0;
+
 // Standard Status Code
 function start() {status = -1; action(1,0,0);}
 function action(mode, type, selection) { if (mode == 1) {status++;} else {status--;} if (status == -1) {cm.dispose();}
@@ -15,7 +18,26 @@ function action(mode, type, selection) { if (mode == 1) {status++;} else {status
         // Make sure it is not empty
         if (buybackItems.size() > 0)
         {
-            cm.sendOk("You have items in your buyback list!");
+            // Create a default text string
+            defaultString = "Sold something on accident did ya? Rookie mistake... What would you like to buy back?\r\n\r\n";
+
+            // Iterate through each item in the list
+            for (var i = 0; i < buybackItems.length; i++)
+            {
+                // Set the current itemId to the missingItem id
+                var itemId = buybackItems[i].itemId;
+
+                // Add to the text string
+                defaultString += "\r\n" + "#L" + selectionSlot + "##v" + itemId + "# #t" + itemId + "##l";
+
+                // Increment the selection #
+                selectionSlot++;
+            }
+
+            // Send the full string w/ selections
+            cm.sendSimple(defaultString);
+
+            // Kill the convo
             cm.dispose();
         }
 
