ac0c0e878972a920669834ce285ebb3785a28e85
anon <anon@example.com>
2025-03-31 14:59:12 -0700

    Ongoing changes...

--- src/main/java/server/maps/MapleMap.java
+++ src/main/java/server/maps/MapleMap.java
@@ -1473,7 +1473,7 @@ public class MapleMap {
 
             // Spawn NPC
             MapleMap source = new MapleMap(mapid, world, channel, returnMapId, monsterRate);
-            Point targetPosition = new Point((int) monster.getPosition().getX(), (int) monster.getPosition().getY() + 5);
+            Point targetPosition = new Point((int) monster.getPosition().getX(), (int) monster.getPosition().getY() - 50);
             spawnNpc(2040036, targetPosition, source);
 
             /*
