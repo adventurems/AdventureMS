217b0f99191cdcffce84b23108d56715b3ad5928
anon <anon@example.com>
2025-04-09 11:55:03 -0700

    Ongoing changes...

--- src/main/java/server/life/Monster.java
+++ src/main/java/server/life/Monster.java
@@ -1648,7 +1648,7 @@ public class Monster extends AbstractLoadedLife {
 
                 // AdventureMS Custom Debugging
                 chr.yellowMessage("Monster dying...");
-                map.killMonster(monster.getObjectId());
+                map.killMonster(monster, chr, true, (short) 1);
                 return;
             }
 
