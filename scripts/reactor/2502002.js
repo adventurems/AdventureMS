2a0e4ca001a1ec75697e9d9a9a03c8bb3470ec45
anon <anon@example.com>
2025-04-16 13:51:11 -0700

    Ongoing changes...

--- scripts/reactor/2502002.js
+++ scripts/reactor/2502002.js
@@ -4,7 +4,7 @@ const PacketCreator = Java.type('tools.PacketCreator');
 function act()
 {
     // Spawn Dyle
-    rm.spawnMonster(9500346, 1, 45, -117)
+    rm.spawnMonster(6220000, 1, 45, -117)
 
     // Message the map!
     rm.getPlayer().getMap().broadcastMessage(PacketCreator.serverNotice(6, "Dyle takes the bait and slithers out of the swamp!"));
