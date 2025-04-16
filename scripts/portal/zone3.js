3bb174b8e0974087af7b8026e97f26329efe25a5
anon <anon@example.com>
2025-04-15 18:05:06 -0700

    Ongoing changes...

--- scripts/portal/zone3.js
+++ scripts/portal/zone3.js
@@ -1,24 +1,5 @@
-/*
-	This file is part of the OdinMS Maple Story Server
-    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
-		       Matthias Butz <matze@odinms.de>
-		       Jan Christian Meyer <vimes@odinms.de>
+// AdventureMS Zone 3
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation version 3 as published by
-    the Free Software Foundation. You may not use, modify or distribute
-    this program under any other version of the GNU Affero General Public
-    License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 function enter(pi)
 {
     if (pi.getPlayer().getZoneProgress() >= 2)
@@ -26,7 +7,9 @@ function enter(pi)
         pi.playPortalSound();
         pi.warp(260010000, "west00");
         return false;
-    } else
+    }
+
+    else
     {
         pi.getPlayer().yellowMessage("You haven't cleared Zone 2! Kerning is in trouble...");
         return false;
