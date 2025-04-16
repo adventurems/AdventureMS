afcbda64e2b19d16600299b75f1344108f472bf0
anon <anon@example.com>
2025-04-16 15:56:01 -0700

    Fix vault monster check, adjust item stats, and boost skill damage.
    Updated logic in vaultcomplete.js for monster checks and conditional warping. Adjusted item and weapon requirements, including reduced DEX for specific items. Increased damage scaling for the "Lucky Seven" skill to improve balance and usability.

--- scripts/portal/MD_vaultcomplete.js
+++ scripts/portal/MD_vaultcomplete.js
@@ -21,22 +21,35 @@
 
 /*
 Inside Vault - Mob Check
-*/ 
+*/
 
 function enter(pi)
 {
-    if (pi.getPlayer().getMap().countMonsters() == 0)
+    if (pi.getPlayer().getMapId() < 198000103)
     {
-        pi.playPortalSound();
-        pi.warp(198000105, "st00");
-        return true;
-    } else
-    {
-        if (pi.getPlayer().getMapId() < 198000103)
+        // Check for specific mob ID 9400633 for maps below 198000103
+        if (pi.getPlayer().getMap().getMonsterById(9400633) == null)
+        {
+            pi.playPortalSound();
+            pi.warp(198000105, "st00");
+            return true;
+        }
+        else
         {
             pi.playerMessage(5, "Astaroth still stands, defeat the vault protector!");
             return false;
-        } else
+        }
+    }
+    else
+    {
+        // Check for any monsters for other maps
+        if (pi.getPlayer().getMap().countMonsters() == 0)
+        {
+            pi.playPortalSound();
+            pi.warp(198000105, "st00");
+            return true;
+        }
+        else
         {
             pi.playerMessage(5, "The cursed adventurers still stand, defeat the vault protectors!");
             return false;
