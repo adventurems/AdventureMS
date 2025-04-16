0f0fb84931c62a0c912bfbeb7e1042ee605d953f
anon <anon@example.com>
2025-04-16 13:16:30 -0700

    Ongoing changes...

--- scripts/reactor/2502002.js
+++ scripts/reactor/2502002.js
@@ -7,5 +7,5 @@ function act()
     rm.spawnMonster(9500346, 1, 45, -117)
 
     // Message the map!
-    rm.getPlayer().getMap().broadcastMessage(PacketCreator.serverNotice(6, "Dyle takes the bait and slithers out of the swamp!"));
+    // rm.getPlayer().getMap().broadcastMessage(PacketCreator.serverNotice(6, "Dyle takes the bait and slithers out of the swamp!"));
 }
