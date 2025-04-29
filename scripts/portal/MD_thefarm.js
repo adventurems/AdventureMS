7f26b024c805515391baa2a27012a1ffbd37f532
anon <anon@example.com>
2025-04-28 21:58:08 -0700

    Zone 4 Quest Updates

--- scripts/portal/MD_thefarm.js
+++ scripts/portal/MD_thefarm.js
@@ -4,7 +4,20 @@ function enter(pi)
 {
     // Leaving the dungeon
     var map = pi.getMapId();
-    pi.getPlayer().dispelDebuffs();
+    var player = pi.getPlayer();
+
+    // Get the event instance the player is in
+    var eim = player.getEventInstance();
+
+    // If the player is in an event instance, unregister them
+    if (eim != null)
+    {
+        eim.unregisterPlayer(player);
+        eim.dispose();
+    }
+
+    // Clean up and warp the player
+    player.dispelDebuffs();
     pi.resetMapObjects(map);
     pi.getMap().clearDrops();
     pi.playPortalSound();
