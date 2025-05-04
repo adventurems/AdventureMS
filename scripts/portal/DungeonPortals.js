5ac5a9e5834781d041f1d5bfce7dff0a2e838817
anon <anon@example.com>
2025-05-04 11:22:01 -0700

    Dungeon Updates

--- scripts/portal/DungeonPortals.js
+++ scripts/portal/DungeonPortals.js
@@ -9,6 +9,8 @@ function enter(pi)
     // Ensure all monsters are defeated
     if (pi.getPlayer().getMap().countMonsters() == 0)
     {
+        // Add debugging
+        pi.getPlayer().yellowMessage("curMap: " + curMap + " | target: " + target + " | Monsters: " + pi.getPlayer().getMap().countMonsters());
         pi.playPortalSound();
         pi.getPlayer().changeMap(target, target.getPortal("in00"));
         return true;
