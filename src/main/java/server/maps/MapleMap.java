4b7fadd1e1dfcc0d8be2b85dacf3734943ea8cde
anon <anon@example.com>
2025-05-05 13:45:30 -0700

    Dungeon Updates, Mob Difficulty Changes

--- src/main/java/server/maps/MapleMap.java
+++ src/main/java/server/maps/MapleMap.java
@@ -567,18 +567,20 @@ public class MapleMap {
 
         // Set the goblin's position to match the original monster's position
         goblin.setPosition(monster.getPosition());
-        monster.getMap().broadcastMessage(PacketCreator.serverNotice(6, "A rare monster has appeared!" + " [Debug] Goblin EXP: " + goblin.getExp()));
+        monster.getMap().broadcastMessage(PacketCreator.serverNotice(6, "[Debug] 1st: Goblin EXP: " + goblin.getExp()));
 
         // Multiply HP by 10 and exp by 7.5
         goblin.changeDifficultyBasicWithStats(monster, 10);
 
-        monster.getMap().broadcastMessage(PacketCreator.serverNotice(6, "A rare monster has appeared!" + " [Debug] Goblin EXP: " + goblin.getExp()));
+        monster.getMap().broadcastMessage(PacketCreator.serverNotice(6, "[Debug] 2nd: Goblin EXP: " + goblin.getExp()));
 
         // Announce the monster spawn to the map
         monster.getMap().broadcastMessage(PacketCreator.playSound("AdventureMS/goblin"));
 
         // Spawn the goblin on the map
         spawnMonster(goblin);
+
+        monster.getMap().broadcastMessage(PacketCreator.serverNotice(6, "[Debug] 3rd: Goblin EXP: " + goblin.getExp()));
     }
 
     public void addSelfDestructive(Monster mob) {
