dbb2bfc0c89b9afdc493acaf2598754ca93e33e8
anon <anon@example.com>
2025-05-07 21:01:58 -0700

    Zone 4 Quest Updates

--- scripts/portal/MD_vault.js
+++ scripts/portal/MD_vault.js
@@ -5,77 +5,63 @@ MiniDungeon - The Vault
 var baseid = 198000000;
 var dungeonid = 198000100;
 var singledungeons = 3;
-var multidungeons = 5;
 
-function enter(pi) {
+function enter(pi)
+{
+    // Get the players job
     var job = pi.getPlayer().getJob();
+
+    // CHeck the base map
     if (pi.getMapId() === baseid)
     {
+        // Make sure they have the key and that they haven't cleared already
         if (pi.getPlayer().haveItem(3997002) && pi.getPlayer().getZoneProgress() <= 2)
         {
+            // Check their party
             if (pi.getParty() != null)
             {
                 pi.playerMessage(5, "You may only access the vault alone!");
                 return false;
             }
+
+            // Party is empty, enter the vault
             else
             {
-                if (job === "MAGICIAN" || job === "BOWMAN" || job === "THIEF" || job === "PIRATE" || job === "WARRIOR")
+                for (var i = 0; i < singledungeons; i++)
                 {
-                    for (var i = 0; i < singledungeons; i++)
+                    if(pi.startDungeonInstance(dungeonid + i))
                     {
-                        if(pi.startDungeonInstance(dungeonid + i))
-                        {
-                            pi.playPortalSound();
-                            pi.warp(dungeonid + i, "west00");
-                            pi.playerMessage(5, "The vault is protected, you must battle your way through!");
-                            pi.resetMapObjects(dungeonid + i);
-                            pi.spawnMonster(9400633, 954, 200);
-                            return true;
-                        }
+                        pi.playPortalSound();
+                        pi.warp(dungeonid + i, "west00");
+                        pi.playerMessage(5, "The vault is protected, you must battle your way through!");
+                        pi.resetMapObjects(dungeonid + i);
+                        pi.spawnMonster(9400633, 954, 200);
+                        return true;
                     }
-
-                    // No open maps, tell the player it's occupied
-                    pi.playerMessage(5, "All vault instances are currently occupied, just a moment!");
-                    return false;
                 }
-                else
-                {
-                    for (var i = 3; i < multidungeons; i++)
-                    {
-                        if(pi.startDungeonInstance(dungeonid + i))
-                        {
-                            pi.playPortalSound();
-                            pi.warp(dungeonid + i, "west00");
-                            pi.playerMessage(5, "The vault is protected, you must battle your way through!");
-                            pi.resetMapObjects(dungeonid + i);
-                            pi.spawnMonster(9400609, 920, 200);
-                            pi.spawnMonster(9400610, 935, 200);
-                            pi.spawnMonster(9400611, 950, 200);
-                            pi.spawnMonster(9400612, 965, 200);
-                            pi.spawnMonster(9400613, 980, 200);
-                            return true;
-                        }
-                    }
 
-                    // No open maps, tell the player it's occupied
-                    pi.playerMessage(5, "All vault instances are currently occupied, just a moment!");
-                    return false;
-                }
-            }
-        } else
-        {
-            if (pi.getPlayer().getZoneProgress() >= 3)
-            {
-                pi.playerMessage(5, "You have already cleared the vault!");
-                return false;
-            } else
-            {
-                pi.playerMessage(5, "You need the Black Vault Key!")
+                // No open maps, tell the player it's occupied
+                pi.playerMessage(5, "All vault instances are currently occupied, just a moment!");
                 return false;
             }
         }
-    } else
+
+        else if (pi.getPlayer().getZoneProgress() >= 3)
+        {
+                pi.playerMessage(5, "You have already cleared the vault!");
+                return false;
+        }
+
+
+        else
+        {
+            pi.playerMessage(5, "You need the Black Vault Key!")
+            return false;
+        }
+    }
+
+    // Leaving the dungeon
+    else
     {
     	var map = pi.getMapId();
         pi.resetMapObjects(map);
