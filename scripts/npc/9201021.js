22a0b5f7a930ef9fd25d9f163227d711f3ff0997
anon <anon@example.com>
2025-05-14 16:59:29 -0700

    Recipe Updates

--- scripts/npc/9201021.js
+++ scripts/npc/9201021.js
@@ -1,11 +1,15 @@
 // AdventureMS Robin the Huntress
 
+// Track glove recipe interaction
+var buyGloveRecipe = false;
+var recipePrice = 2500000;
+
 // Standard Status Code
 function start() {status = -1; action(1,0,0);}
-function action(mode, type, selection) { if (mode == 1) {status++;} else {status--;} if (status == -1) {cm.dispose();}
+function action(mode, type, selection) { if (mode === 1) {status++;} else {status--;} if (status === -1) {cm.dispose();}
 
     // Initial Click
-    else if (status == 0)
+    else if (status === 0)
     {
         // Check if the quest is completed
         if (cm.getQuestStatus(1014) < 2)
@@ -25,38 +29,77 @@ function action(mode, type, selection) { if (mode == 1) {status++;} else {status
         }
 
         // The original quest is completed, see who they chose
-        else if (cm.getQuestStatus(1017) == 2) // Robin was chosen
+        else if (cm.getQuestStatus(1017) === 2) // Robin was chosen
         {
             cm.sendOk("How do I look? Are you happy with your choice, hun?");
             cm.dispose();
         }
 
         // They chose Corine to give gear to
-        else if (cm.getQuestStatus(1018) == 2)
+        else if (cm.getQuestStatus(1018) === 2)
         {
-            cm.sendOk("I can't believe it. Do NOT talk to me again.");
-            cm.dispose();
+            cm.sendYesNo("I can't believe it. Do NOT talk to me again. Well, at least she doesn't have these #rfancy gloves#k I found. They make my hands look so nice...\r\n\r\nOh? You want to know how I got them?" +
+            "I suppose I could share how for a price... If you'd chosen me I would have given the recipe up freely!\r\n\r\nHow about #r" + recipePrice + "#k mesos? How does that sound?");
+
+            // They may have selected yes
+            buyGloveRecipe = true;
         }
     }
 
     // After pressing yes/next
-    else if (status == 1)
+    else if (status === 1)
     {
-        // They do have the items and chose to turn them in
-        cm.sendOk("Amazing, I'm going to look great in these for you!\r\n\r\nHave this as a token of my appreciation!\r\n#v1142203# #t1142203#");
+        // They tried to buy the recipe
+        if (buyGloveRecipe)
+        {
+            // Check that they have enough mesos
+            if (cm.getMeso >= recipePrice)
+            {
+                // Check that they can hold it
+                if (cm.canHold(4007033))
+                {
+                    cm.gainMeso(-recipePrice); // Take Mesos
+                    cm.gainItem(4007033, 1); // Give Recipe
+                    cm.sendOk("Here, take the recipe, don't talk to me again...");
+                    cm.completeQuest()
+                }
 
-        // Give/take items
-        cm.gainItem(1041061, -1); // Remove top
-        cm.gainItem(1061057, -1); // Remove bottom
-        cm.gainItem(1142203, 1); // Robin Medal
-        cm.gainExp(75000); // Give exp
-        cm.gainFame(1); // Give fame
+                // They can't hold it
+                else
+                {
+                    cm.sendOk("You need room in your #rETC#k inventory to hold the recipe... Not prepared, I'd expect nothing more from you...");
+                }
+            }
 
-        // Complete the quest
-        cm.completeQuest(1014); // Dual quest
-        cm.completeQuest(1017); // Robin Specific Quest
+            // They don't have enough mesos
+            else
+            {
+                cm.sendOk("Bahahahaha, you don't have enough mesos to buy the recipe... Why am I not surprised?");
+            }
 
-        // Kill convo
-        cm.dispose();
+            // Done talking no matter what
+            cm.dispose();
+        }
+
+        // They are turning in gear
+        else
+        {
+            // They do have the items and chose to turn them in
+            cm.sendOk("Amazing! I'm going to look great in these for you!\r\n\r\nHave this as a token of my appreciation!\r\n#v1142203# #t1142203#");
+
+            // Give/take items
+            cm.gainItem(1041061, -1); // Remove top
+            cm.gainItem(1061057, -1); // Remove bottom
+            cm.gainItem(1142203, 1); // Robin Medal
+            cm.gainExp(75000); // Give exp
+            cm.gainFame(1); // Give fame
+
+            // Complete the quest
+            cm.completeQuest(1014); // Dual quest
+            cm.completeQuest(1017); // Robin Specific Quest
+
+            // Kill convo
+            cm.dispose();
+        }
     }
 }
