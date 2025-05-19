364149f507362e819bf0be40ee1bbd50fcd7899c
anon <anon@example.com>
2025-05-18 22:01:24 -0700

    Zone 4 fixes

--- scripts/npc/1052008.js
+++ scripts/npc/1052008.js
@@ -2,54 +2,71 @@
 
 function start()
 {
-    // Check that we have started Hill's quest
-    if (cm.haveItem(3997006, 1))
+
+}
+
+// Standard Status Code
+function start() {status = -1; action(1,0,0);}
+function action(mode, type, selection) { if (mode === 1) {status++;} else {status--;} if (status === -1) {cm.dispose();}
+
+    // Initial Click
+    else if (status === 0)
     {
-        // Check that we can hold the Moon Rock
-        if (cm.canHold(4011007, 1))
+        // Check that we have started Hill's quest
+        if (cm.haveItem(3997006, 1))
         {
-            // Gain Items
-            cm.gainItem(4011007, 1); // Moon Rock
-            cm.gainItem(3997006, -1) // Take the Map
-            cm.gainMeso(1000000); // Mesos
-            cm.gainExp(75000); // Exp
+            // Check that we can hold the Moon Rock
+            if (cm.canHold(4011007, 1))
+            {
+                // Send Message
+                cm.sendNext("You found the chest! You eagerly break it open to find:\r\n#v5200002# 1,000,000 Mesos\r\n#v4011007# #t4011007#!");
+            }
 
-            // Complete Quest
-            cm.completeQuest(1016);
+            // Their ETC inventory is full
+            else
+            {
+                cm.sendOk("You don't have room in your #rETC#k inventory to receive the item!");
+                cm.dispose();
+            }
+        }
 
-            // Send Message
-            cm.sendOk("You found the chest! You eagerly break it open to find:\r\n#v5200002# 1,000,000 Mesos\r\n#v4011007# #t4011007#!");
-            cm.warp(100000203, "west00");
-
-            // Kill convo
+        // They've already looted the chest
+        else if (cm.getQuestStatus(1016) === 2)
+        {
+            cm.sendOk("You've already looted the chest!");
             cm.dispose();
         }
 
-        // Their ETC inventory is full
+        // They haven't started the Hill quest yet and don't have the map
+        else if (cm.getQuestStatus(1016) === 0)
+        {
+            cm.sendOk("You haven't traded for the map from #bHill#k yet!");
+            cm.dispose();
+        }
+
         else
         {
-            cm.sendOk("You don't have room in your #rETC#k inventory to receive the item!");
+            cm.sendOk("Something isn't right, report this in Discord! (1052008)");
             cm.dispose();
         }
     }
 
-    // They've already looted the chest
-    else if (cm.getQuestStatus(1016) === 2)
+    // After pressing yes/next
+    else if (status === 1)
     {
-        cm.sendOk("You've already looted the chest!");
-        cm.dispose();
-    }
+        // Gain Items
+        cm.gainItem(4011007, 1); // Moon Rock
+        cm.gainItem(3997006, -1) // Take the Map
+        cm.gainMeso(1000000); // Mesos
+        cm.gainExp(75000); // Exp
 
-    // They haven't started the Hill quest yet and don't have the map
-    else if (cm.getQuestStatus(1016) === 0)
-    {
-        cm.sendOk("You haven't traded for the map from #bHill#k yet!");
-        cm.dispose();
-    }
+        // Complete Quest
+        cm.completeQuest(1016);
 
-    else
-    {
-        cm.sendOk("Something isn't right, report this in Discord! (1052008)");
+        // Warp back to Kora
+        cm.warp(100000203, "west00");
+
+        // Kill convo
         cm.dispose();
     }
 }
