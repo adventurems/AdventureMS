afca5dc773138c239261162306c210464fd24f2e
anon <anon@example.com>
2025-05-19 21:57:01 -0700

    Zone 4 fixes

--- scripts/quest/1034.js
+++ scripts/quest/1034.js
@@ -14,7 +14,16 @@ function end(mode, type, selection) { if (mode === 1) {status++;} else {status--
         // Heart check
         if (qm.haveItem(4007030))
         {
-            qm.sendNext("The heart! You have it. Let me see it, quick!\\n\\n(#bMoki#k holds it against the black heart, pulls back his arm, hammer in hand and gently taps the heart with his eyes closed. He and the heart begin to glow. Nothing happens, but then... you hear a feint noise...)\\n\\n(It starts slow and quiet................ thump............... thump........... thump...... thump.. thump)\\n\\n(It builds up and the room begins to swirl! The heart breaks free from it's black casing. The glass shards shatter all around you. As you open your eyes, you are greeted with a beautiful blue shine. The heart lives on and the #Manaforge#k lives once more!!!)");
+            if (qm.canHold(1142205))
+            {
+                qm.sendNext("The heart! You have it. Let me see it, quick!\\n\\n(#bMoki#k holds it against the black heart, pulls back his arm, hammer in hand and gently taps the heart with his eyes closed. He and the heart begin to glow. Nothing happens, but then... you hear a feint noise...)\\n\\n(It starts slow and quiet................ thump............... thump........... thump...... thump.. thump)\\n\\n(It builds up and the room begins to swirl! The heart breaks free from it's black casing. The glass shards shatter all around you. As you open your eyes, you are greeted with a beautiful blue shine. The heart lives on and the #Manaforge#k lives once more!!!)");
+            }
+
+            else
+            {
+                qm.sendOk("Make room for the medal!");
+                qm.dispose();
+            }
         }
 
         else
@@ -28,6 +37,10 @@ function end(mode, type, selection) { if (mode === 1) {status++;} else {status--
     else if (status === 2)
     {
         qm.warp(106010116, "in00");
+        qm.gainItem(4007030, -1); // Take heart
+        qm.gainItem(1142205, 1); // Gain medal
+        qm.gainExp(150000); // Gain exp
+        qm.getPlayer().updateZoneProgress(); // increase zone progress
         qm.sendOk("You did it! You solved the mysteries of the ruins. You crafted the #rHeart of the Forge#k, proved your worth as an aspiring forge master and conquered #bZone 4#k!");
         qm.dispose();
     }
