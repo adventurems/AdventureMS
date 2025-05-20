beaa0e59252f2c573b03dcd57332a8210d1d9a5f
anon <anon@example.com>
2025-05-19 21:39:29 -0700

    Zone 4 fixes

---
+++ scripts/quest/0000.js
@@ -0,0 +1,22 @@
+// Standard Status Code
+function start() {status = -1; action(1,0,0);}
+function action(mode, type, selection) { if (mode === 1) {status++;} else {status--;} if (status === -1) {qm.dispose();}
+
+    // Start
+    else if (status === 0)
+    {
+
+    }
+
+    // After pressing yes/next
+    else if (status === 1)
+    {
+
+    }
+
+    // After Advancing one further
+    else if (status === 2)
+    {
+
+    }
+}
