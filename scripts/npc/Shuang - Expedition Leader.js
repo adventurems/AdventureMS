74dcea1bccd676ed8dfa3c178cd2b6746a057513
anon <anon@example.com>
2025-05-19 20:30:03 -0700

    Zone 4 fixes

--- scripts/npc/Shuang - Expedition Leader.js
+++ scripts/npc/Shuang - Expedition Leader.js
@@ -7,7 +7,8 @@ function action(mode, type, selection) { if (mode === 1) {status++;} else {statu
     // Initial Click
     else if (status === 0)
     {
-        cm.sendOk("Hello, I'm Shuang. I'm here to help you with your quests. If you have any questions, please ask me.");
+        cm.sendOk("Hello, I'm Shuang. I lead the expedition here at the #bManaforge#k. I work closely with #bMoki#k the forge master.\r\n\r\n" +
+        "The forge is not well these days. The heart has turned black and the the automatons run rampant across the land. Hopefully you are the one we need. So many others have come and fall before you.");
         cm.dispose();
     }
 
