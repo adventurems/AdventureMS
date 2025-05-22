12265457da10557744e58bf69a6ee1154952cbcb
anon <anon@example.com>
2025-05-21 20:18:08 -0700

    Zone 4 fixes

--- scripts/npc/9800029.js
+++ scripts/npc/9800029.js
@@ -7,7 +7,7 @@ function start() {status = -1; action(1,0,0);}
 function action(mode, type, selection) { if (mode == 1) {status++;} else {status--;} if (status == -1) {cm.dispose();}
 
     // Initial Click
-    else if (status == 0)
+    else if (status === 0)
     {
         // Check if they have completed the quest
         if (cm.getQuestStatus(1019) === 1)
